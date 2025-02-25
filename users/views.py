6f8108262fe0a9692b65ef92cfb6a39778b1c632
Aziz-Ibrahim <dsnxc622@students.codeinstitute.net>
2025-02-25 21:42:53 +0000

    Create users app and wire up user authentication from django

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import RegisterForm
 
-# Create your views here.
+def home(request):
+    return render(request, "users/home.html")
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Automatically log the user in
+            return redirect("home")  # Redirect to homepage
+    else:
+        form = RegisterForm()
+    return render(request, "users/register.html", {"form": form})
+
