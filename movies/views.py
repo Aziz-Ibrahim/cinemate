461d5d30b09e4d02a9803cfa070e20d3505956a7
Aziz-Ibrahim <dsnxc622@students.codeinstitute.net>
2025-02-27 16:29:03 +0000

    Create a movie display: Added card layout with movie details and linked CSS styles for better presentation.

--- movies/views.py
+++ movies/views.py
@@ -1,21 +1,15 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 import requests
-from django.shortcuts import render
 from django.conf import settings
-
 
 @login_required
 def movie_list(request):
-    return render(request, "movies/movie_list.html")
-
+    movies = get_popular_movies()
+    return render(request, "movies/movie_list.html", {"movies": movies})
 
 def get_popular_movies():
     url = "https://api.themoviedb.org/3/movie/popular"
     params = {"api_key": settings.TMDB_API_KEY, "language": "en-US", "page": 1}
     response = requests.get(url, params=params)
     return response.json().get("results", [])
-
-def movie_list(request):
-    movies = get_popular_movies()
-    return render(request, "movies/movie_list.html", {"movies": movies})
