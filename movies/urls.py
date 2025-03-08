76d9bf4054b118e32250e815ce98d90fe95ebe45
Aziz-Ibrahim <dsnxc622@students.codeinstitute.net>
2025-03-08 00:09:03 +0000

    fix; Add an if condition to stop system crashing if it fails to retrieve movie detiails

--- movies/urls.py
+++ movies/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("", movie_list, name="movie_list"),
     path("<int:movie_id>/", movie_detail, name="movie_detail"),
     path("toggle_favorite/", toggle_favorite, name="toggle_favorite"),
-    path("reviews/", include("reviews.urls")),  # Include reviews URLs
+    path("reviews/", include("reviews.urls")),  
 ]
